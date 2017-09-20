15dacb7030481b536418c3c35850887dd7810e0a
Danjo <danjobilek@gmail.com>
2017-09-20 15:33:02 +0200

    Finished with rotational cipher

--- lib/rotational_cipher.rb
+++ lib/rotational_cipher.rb
@@ -1,3 +1,53 @@
 class RotationalCipher
+  def rot(word, number)
+    @word = word
+    @number = number.to_i
+    array = []
+    if @number < 0 || @number > 26
+      puts "The number you're entered must be from 0 to 26, please enter a valid number"
+    else
+      @word.each_char.map do |letter|
+        if letter =~ /[A-Za-z]/
+           letter = check_letter(letter, @number)
+           array << convert_to_ascii_letter(letter)
+        else
+        array << letter
+        end
+      end
+    end
+    result = array.join
+    result
+  end
 
+  def convert_to_ascii_num(letter)
+    ascii_number = letter.ord
+    ascii_number
+  end
+
+  def convert_to_ascii_letter(number)
+    letter = number.chr
+    letter
+  end
+
+  def check_letter(letter, number)
+    ascii_number =  letter =~ /[A-Z]/ ? 64 : 96
+    letter = convert_to_ascii_num(letter)
+    letter -= ascii_number
+    if number == 26
+      letter
+    elsif (letter + number) > 26
+      if letter > number
+        letter -= number
+      elsif letter == number
+        letter = 26 - letter
+      else
+        letter = number - letter
+      end
+    else
+      letter += number
+    end
+      letter += ascii_number
+      letter
+  end
 end
+
--- spec/rotational_cipher_spec.rb
+++ spec/rotational_cipher_spec.rb
@@ -1,4 +1,46 @@
 require "rotational_cipher"
 
 describe RotationalCipher do
+  rc = RotationalCipher.new
+  
+  describe "#rot" do
+    context "given omg and '5'" do
+      it "returns trl" do
+        expect(rc.rot("omg",5)).to eql("trl")
+      end
+    end
+  end
+
+  describe "#rot" do
+    context "given c and '0'" do
+      it "returns c" do
+        expect(rc.rot("c",0)).to eql("c")
+      end
+    end
+  end
+
+  describe "#rot" do
+    context "given Cool and '26'" do
+      it "returns Cool" do
+        expect(rc.rot("Cool",26)).to eql("Cool")
+      end  
+    end
+  end
+
+  describe "#rot" do
+    context "given The quick brown fox jumps over the lazy dog. and '13'" do
+      it "returns Gur dhvpx oebja sbk whzcf bire gur ynml qbt." do
+        expect(rc.rot("The quick brown fox jumps over the lazy dog.",13)).to eql("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.")
+      end  
+    end
+  end
+
+  describe "#rot" do
+    context "given Gur dhvpx oebja sbk whzcf bire gur ynml qbt. and '13'" do
+      it "returns The quick brown fox jumps over the lazy dog." do
+        expect(rc.rot("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.",13)).to eql("The quick brown fox jumps over the lazy dog.")
+      end  
+    end
+  end
 end
+
